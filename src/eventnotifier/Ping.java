1313a92576700de3db2a2f1b6e6a28419e227ea5
Nasibulin <k.nasibulin@yandex.ru>
2018-11-13 15:39:53 +0300

    -

---
+++ src/eventnotifier/Ping.java
@@ -0,0 +1,11 @@
+package eventnotifier;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Nasibulin
+ * Date: 13.11.18
+ * Time: 14:36
+ * To change this template use File | Settings | File Templates.
+ */
+public class Ping {
+}
---
+++ src/eventnotifier/Server.java
@@ -0,0 +1,11 @@
+package eventnotifier;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Nasibulin
+ * Date: 13.11.18
+ * Time: 15:56
+ * To change this template use File | Settings | File Templates.
+ */
+public class Server {
+}
